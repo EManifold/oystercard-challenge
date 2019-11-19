069e3c04e69102af9d4a44d9d00843f332c25a23
Ellie <elliemmanifold@gmail.com>
2019-11-19 15:33:40 +0000

    Add journey history

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -3,12 +3,12 @@ class Oystercard
   BALANCE_LIMIT = 90
   MINIMUM_FARE = 1
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journey_history
 
   def initialize
     @balance = 0
     @entry_station = nil
-
+    @journey_history = []
   end
 
   def top_up(amount)
@@ -22,14 +22,15 @@ class Oystercard
 
   def touch_in(station)
     raise "Insufficient funds" if balance < MINIMUM_FARE
-    @in_journey = true
     @entry_station = station
   end
 
-  def touch_out
+  def touch_out(station)
      deduct(MINIMUM_FARE)
+     @journey_history.push({entry_station: entry_station, exit_station: station})
      @entry_station = nil
   end
+
 
   private
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,13 +4,16 @@ describe Oystercard do
 
   let(:station) { double :station }
   let(:oystercard_full) { Oystercard.new }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
+  let(:journey) { {entry_station: entry_station, exit_station: exit_station} }
 
   before(:each) do
     oystercard_full.top_up(Oystercard::BALANCE_LIMIT)
   end
 
    describe '#balance' do
-     it 'balance on card should be 0' do
+     it 'should be 0' do
        expect(subject.balance).to eq 0
      end
    end
@@ -34,25 +37,27 @@ describe Oystercard do
   end
 
   describe '#in_journey?' do
+     before(:each) do
+       oystercard_full.touch_in(station)
+     end
+
      it "returns true if the oystercard is mid-journey" do
-        oystercard_full.touch_in(station)
-        expect(oystercard_full).to be_in_journey
+       expect(oystercard_full).to be_in_journey
      end
 
      it 'returns false if the oystercard is not mid-journey' do
-        subject.touch_out
-        is_expected.not_to be_in_journey
+       oystercard_full.touch_out(station)
+       expect(oystercard_full).to_not be_in_journey
      end
   end
 
   describe "#touch_in" do
-
     it "throws an error if balance is < 1 when touching in" do
       allow(subject).to receive(:balance) { 0 }
       expect{subject.touch_in(station)}.to raise_error("Insufficient funds")
     end
 
-    it "remember the entry station" do
+    it "remembers the entry station" do
       oystercard_full.touch_in(station)
       expect(oystercard_full.entry_station).to eq station
     end
@@ -61,14 +66,33 @@ describe Oystercard do
   describe '#touch_out' do
     it "reduces the balance by the minimum fare when you touch out" do
         oystercard_full.touch_in(station)
-       expect {oystercard_full.touch_out}.to change{oystercard_full.balance}.by(-Oystercard::MINIMUM_FARE)
-    end
-
-    it "forgets entry station" do
-        oystercard_full.touch_in(station)
-        oystercard_full.touch_out
-        expect(oystercard_full.entry_station).to eq nil
+       expect {oystercard_full.touch_out(station)}.to change{oystercard_full.balance}.by(-Oystercard::MINIMUM_FARE)
     end
   end
 
+  describe '#journey_history' do
+    it 'should start empty' do
+      expect(subject.journey_history).to be_empty
+    end
+  end
+
+  context 'once completed a journey' do
+    it 'the journey is added to journey history' do
+      oystercard_full.touch_in(entry_station)
+      oystercard_full.touch_out(exit_station)
+      expect(oystercard_full.journey_history).to include journey
+    end
+
+    it 'there should be one journey in journey history' do
+      oystercard_full.touch_in(station)
+      oystercard_full.touch_out(station)
+      expect(oystercard_full.journey_history.length).to eq 1
+    end
+
+    it "the entry station returns to nil" do
+        oystercard_full.touch_in(station)
+        oystercard_full.touch_out(station)
+        expect(oystercard_full.entry_station).to eq nil
+    end
+  end
 end
